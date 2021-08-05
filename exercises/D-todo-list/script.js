921a57f725d954c30722a23ce3bdea9283f4edfe
Megumi Yanaka <margaret_mori0102@yahoo.com>
2021-08-05 16:08:37 +0100

    I finished by "adding line-through" event

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,6 +1,25 @@
 function todoList(todos) {
   // Write your code here...
+let content = document.getElementById("content");
+let unorderlistTag = document.createElement("ul");
+content.appendChild(unorderlistTag);
+
+todos.forEach(task => {
+  let listTag = document.createElement("li");
+  unorderlistTag.appendChild(listTag);
+  listTag.innerText = task.todo;
+
+  listTag.addEventListener("click", makeLine);
+  function makeLine(){
+    listTag.style.textDecorationLine = "line-through"
+  }
+
+  // listTag.addEventListener("click", removeLine);  
+  // function removeLine(){
+  //   listTag.style.textDecorationLine = "none"
+  // }
   
+});
 }
 
 const todos = [
