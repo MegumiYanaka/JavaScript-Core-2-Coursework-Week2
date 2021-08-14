477942b0b6cf9c2797006cd9d64ba31688fac379
Megumi Yanaka <margaret_mori0102@yahoo.com>
2021-08-14 16:22:51 +0100

    finised E

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,20 @@
 function listOfColours(colours) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let select= document.createElement("select");
+  let p= document.createElement("p");
+  select.addEventListener("change", evt =>{
+      p.style.backgroundColor = evt.target.options[evt.target.options.selectedIndex].text;
+      p.innerText = evt.target.options[evt.target.options.selectedIndex].text;
+    });
+  content.appendChild(select);
+  content.appendChild(p);
+  colours.forEach(color=>{
+    let option = document.createElement("option");
+    select.appendChild(option);
+    option.text = color;
+ 
+  });
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
